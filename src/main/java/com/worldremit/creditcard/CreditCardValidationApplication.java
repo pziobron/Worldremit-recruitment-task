007f40c880e6378b1b616394ae21be50ed9f1c10
patryk.ziobron <patryk.ziobron@openjawtech.com>
2021-01-23 22:28:44 +0100

    Improvement proposal - remove unused dependency

--- src/main/java/com/worldremit/creditcard/CreditCardValidationApplication.java
+++ src/main/java/com/worldremit/creditcard/CreditCardValidationApplication.java
@@ -1,15 +1,10 @@
 package com.worldremit.creditcard;
 
-import com.worldremit.creditcard.config.GeneralConfig;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class CreditCardValidationApplication {
-
-    @Autowired
-    private GeneralConfig generalConfig;
 
     public static void main(String[] args) {
         SpringApplication.run(CreditCardValidationApplication.class, args);
